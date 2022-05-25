429665836749d8f471d5ef8471b35ff2396d300d
Alan Costa Facchini <costafacchini@gmail.com>
2022-05-25 20:28:33 -0300

    Allow update todo item

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,12 +1,36 @@
 class TodoItemsController < ApplicationController
+  before_action :set_todo_item, only: %i[ edit update ]
+
   def create
     @todo = Todo.find(params[:todo_id])
     @todo_item = @todo.todo_items.create(todo_item_params)
     redirect_to todo_path(@todo)
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @todo_item.update(todo_item_params)
+        format.turbo_stream
+        format.html { redirect_to todo_todo_item_url(@todo_item), notice: 'Todo item was successfully updated.' }
+        format.json { render :show, status: :ok, location: @todo_item }
+      else
+        format.turbo_stream
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @todo_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
+    def set_todo_item
+      @todo_item = TodoItem.find(params[:id])
+      @todo = @todo_item.todo
+    end
+
     def todo_item_params
-      params.require(:todo_item).permit(:description)
+      params.require(:todo_item).permit(:description, :done)
     end
 end
--- spec/routing/todo_items_routing_spec.rb
+++ spec/routing/todo_items_routing_spec.rb
@@ -3,35 +3,35 @@ require 'rails_helper'
 RSpec.describe TodoItemsController, type: :routing do
   describe 'routing' do
     it 'does not route to #index' do
-      expect(get: '/todos/1/todo_items').not_to route_to('todo_items#index')
+      expect(get: '/todos/1/todo_items').not_to route_to('todo_items#index', todo_id: '1')
     end
 
     it 'does not route to #new' do
-      expect(get: '/todos/1/todo_items/new').not_to route_to('todo_items#new')
+      expect(get: '/todos/1/todo_items/new').not_to route_to('todo_items#new', todo_id: '1')
     end
 
     it 'does not route to #show' do
       expect(get: '/todos/1/todo_items/1').not_to route_to('todo_items#show', id: '1')
     end
 
-    it 'does not route to #edit' do
-      expect(get: '/todos/1/todo_items/1/edit').not_to route_to('todo_items#edit', id: '1')
+    it 'routes to #edit' do
+      expect(get: '/todos/1/todo_items/1/edit').to route_to('todo_items#edit', todo_id: '1', id: '1')
     end
 
     it 'routes to #create' do
       expect(post: '/todos/1/todo_items').to route_to('todo_items#create', todo_id: '1')
     end
 
-    it 'does not route to #update via PUT' do
-      expect(put: '/todos/1/todo_items/1').not_to route_to('todo_items#update', id: '1')
+    it 'routes to #update via PUT' do
+      expect(put: '/todos/1/todo_items/1').to route_to('todo_items#update', todo_id: '1', id: '1')
     end
 
-    it 'does not route to #update via PATCH' do
-      expect(patch: '/todos/1/todo_items/1').not_to route_to('todo_items#update', id: '1')
+    it 'routes to #update via PATCH' do
+      expect(patch: '/todos/1/todo_items/1').to route_to('todo_items#update', todo_id: '1', id: '1')
     end
 
     it 'does not route to #destroy' do
-      expect(delete: '/todos/1/todo_items/1').not_to route_to('todo_items#destroy', id: '1')
+      expect(delete: '/todos/1/todo_items/1').not_to route_to('todo_items#destroy', todo_id: '1', id: '1')
     end
   end
 end
